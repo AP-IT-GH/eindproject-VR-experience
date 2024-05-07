89080901fdfda9d827b873bad818041f9a7a2349
inge129116 <113992394+inge129116@users.noreply.github.com>
2024-05-07 14:07:23 +0200

    verander draaien ml-agent en voeg springen toe

--- Assets/scripts/capsuleAgent.cs
+++ Assets/scripts/capsuleAgent.cs
@@ -20,12 +20,26 @@ public class capsuleAgent : Agent
 
     }
     public float speedMultiplier = 0.1f;
-    public override void OnActionReceived(ActionBuffers actionBuffers) {
+    public float rotationmultiplier = 1f;
+    public float jumpForce = 5f;
+    public Rigidbody rb;
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+
         Vector3 controlSignal = Vector3.zero;
-        controlSignal.x = actionBuffers.ContinuousActions[0];
+
         controlSignal.z = actionBuffers.ContinuousActions[1];
         transform.Translate(controlSignal * speedMultiplier);
+        transform.Rotate(0.0f, rotationmultiplier * actionBuffers.ContinuousActions[0], 0.0f);
+        float jumpAction = actionBuffers.ContinuousActions[2];
 
+        if (jumpAction > 0.5f && transform.position.y <= 2)
+        {
+            
+
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            
+        }
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -33,5 +47,6 @@ public class capsuleAgent : Agent
         var continuousActionsOut = actionsOut.ContinuousActions;
         continuousActionsOut[0] = Input.GetAxis("Horizontal");
         continuousActionsOut[1] = Input.GetAxis("Vertical");
+        continuousActionsOut[2] = Input.GetKey(KeyCode.Space) ? 1f : 0f; ;
     }
 }
